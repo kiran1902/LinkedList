301f134a01ad69b6d627b35a680f7021fdd86425
kiran1902 <kiru2kumar@gmail.com>
2022-04-11 00:02:59 +0530

    To add element 40 after 30 into the linked list

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -4,14 +4,16 @@ Console.WriteLine("Welcome to LinkedList Program");
 
 
 /// <summary>
-/// UC7 - To search an element in linked list
-/// /// </summary>
+/// UC8 - Adding an element(40 after 30) in linked list
+/// </summary>
 
 LinkedLis<int> myList = new LinkedLis<int>();
 myList.Add(56);
 myList.Add(30);
 myList.Add(70);
 
-Console.WriteLine(myList.Search(30));
-
+Console.WriteLine("Before Adding");
+myList.Display();
+myList.Pat_Pos(40, 3);
+Console.WriteLine("After adding");
 myList.Display();
