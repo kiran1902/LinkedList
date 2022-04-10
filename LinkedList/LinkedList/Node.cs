c2b9ce3102c3dd9c681d3cf6d12efccefde21a20
kiran1902 <kiru2kumar@gmail.com>
2022-04-10 22:29:35 +0530

    Simple Linked List

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node<Gtype>
+    {
+        public Gtype val;
+        public Node<Gtype> next;
+
+        public Node(Gtype Data)
+        {
+            this.val = Data;
+            this.next = null;
+        }
+    }
+}
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using LinkedList;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to LinkedList Program");
+
+
+/// <summary>
+/// UC1 - Insert a element at last in linked list
+/// /// </summary>
+
+LinkedLis<int> myList = new LinkedLis<int>();
+myList.Add(56);
+myList.Add(30);
+myList.Add(70);
+myList.Display();
